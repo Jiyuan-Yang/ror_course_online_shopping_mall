8c19f655415ef6338d805371e4e67ea887da2b8a
Jiyuan-Yang <jim_9827@icloud.com>
2019-10-22 22:43:22 +0800

    add product show

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validates :description, presence: true, length: {minimum: 10, maximum: 100}
 
-  VALID_PRICE_REGEX = /\A\d+(\.\d{1,2})\z/
+  # VALID_PRICE_REGEX = /\A\d+(\.\d{1,2})\z/
   validates :price, presence: true
   validates_numericality_of :price
   # use this to judge whether if it's a number
