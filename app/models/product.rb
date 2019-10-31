86eb8eaf80a9d23fbae28f5526f40d99f57ad0e0
Jiyuan-Yang <jim_9827@icloud.com>
2019-10-31 13:28:22 +0800

    add check for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
+  validate :partial_unique_validation
   validates :description, presence: true, length: {minimum: 10, maximum: 100}
 
   # VALID_PRICE_REGEX = /\A\d+(\.\d{1,2})\z/
@@ -11,4 +12,18 @@ class Product < ApplicationRecord
   has_many :shopping_cart_items
   has_many :favorite_items
   has_many :order_items
+
+  private
+  def partial_unique_validation
+    cur_shop_products = Shop.find(shop_id).products
+    judge = 0
+    cur_shop_products.each do |item|
+      if name == item.name
+        judge = 1
+      end
+    end
+    if judge == 1
+      errors[:name] << 'Product name is already used in this shop'
+    end
+  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ApplicationRecord
-  validates :name, presence: true, length: {maximum: 30}
+  validates :name, presence: true, length: {maximum: 30},
+            uniqueness: {case_sensitive: true}
   validates :description, presence: true, length: {minimum: 10, maximum: 100}
 
   belongs_to :user
