a251f6918d1e204538016e172bbca73b1c627ffa
maxu <17373191@buaa.edu.cn>
2019-11-11 23:24:11 +0800

    add category to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_044951) do
+ActiveRecord::Schema.define(version: 2019_11_11_145849) do
 
   create_table "favorite_items", force: :cascade do |t|
     t.integer "product_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_044951) do
     t.integer "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "shopping_cart_items", force: :cascade do |t|
