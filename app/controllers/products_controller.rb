9fdf2dbce4de75894dd9096057f46db2b3cab5d1
Jiyuan-Yang <jim_9827@icloud.com>
2019-12-05 17:06:37 +0800

    bug fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :monthly, :sales_ranking, :destroy]
-  before_action :correct_user_seller, only: [:new, :create, :sales_ranking, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :monthly, :sales_ranking]
+  before_action :correct_user_seller, only: [:new, :create, :sales_ranking]
   before_action :product_current_user?, only: [:monthly]
 
   protect_from_forgery :except => :index
