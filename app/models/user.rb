96f8f85e2ac64a028e203cd70012d370a8f2d7a0
maxu <17373191@buaa.edu.cn>
2019-12-05 18:18:11 +0800

    fix bugs of user's information

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
-  VALID_RECEIVER_NAME_REGEX = /\A[a-z]*\z/i
-  validates :receiver_name, format: {with: VALID_RECEIVER_NAME_REGEX, message: 'Your name must be character between a-z'}
+  validates :receiver_name, presence: true
   VALID_RECEIVER_PHONE_REGEX = /\A(\d{11})?\z/i
-  validates :receiver_phone_number, format: {with: VALID_RECEIVER_PHONE_REGEX, message: 'The phone number should contain 11 numbers'}
+  validates :receiver_phone_number, format: {with: VALID_RECEIVER_PHONE_REGEX, message: '手机号应该包含11位数字'}
 
   has_many :shops, dependent: :delete_all
   has_one :shopping_cart, dependent: :destroy
